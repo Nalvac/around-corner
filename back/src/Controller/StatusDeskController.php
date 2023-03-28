8a862dc15be95f92d319fca5c9e2ac82c2027b9b
Amine Fajry <amine.f@dalvin.fr>
2023-03-28 16:19:16 +0200

    Create StatusDeskController.php

---
+++ back/src/Controller/StatusDeskController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class StatusDeskController extends AbstractController
+{
+    #[Route(path: 'api/status-user', name: 'api_status', methods: ['GET'])]
+    public function options(StatusRepository $statusUsersRepository): JsonResponse
+    {
+        $models = $statusUsersRepository->findAll();
+        foreach ($models as $status) {
+            $data[] = [
+                'id' => $status->getId(),
+                'name' => $status->getName()
+            ];
+        }
+
+        return new JsonResponse($data,Response::HTTP_OK);
+    }
+
+    #[Route(path: 'api/status-user/{id}', name: 'api_delete_status', methods: ['DELETE'])]
+    public function deleteOption(StatusUsersRepository $statusUsersRepository, string $id): JsonResponse
+    {
+        $status = $statusUsersRepository->findOneById($id);
+        if (!$status) {
+            throw new \Exception('Sorry, status does not exist', Response::HTTP_NOT_FOUND);
+        }
+
+        // Supprimer la référence de statut dans tous les utilisateurs associés
+        $users = $status->getUsers();
+        foreach ($users as $user) {
+            $user->setStatus(null);
+        }
+
+        $statusUsersRepository->remove($status,true);
+        return new JsonResponse(
+            [
+                'message' => "Status is deleted",
+            ], Response::HTTP_OK
+        );
+    }
+
+    #[Route(path: 'api/status-user/{id}', name: 'api_update_status', methods: ['PUT'])]
+    public function editOption(StatusUsersRepository $statusUsersRepository, Request $request, string $id): JsonResponse
+    {
+        $status = $statusUsersRepository->findOneBy(['id' => $id]);
+        if ($status == null) {
+            throw new \Exception('Sorry, status does not exist', Response::HTTP_NOT_FOUND);
+        }
+
+        $data = json_decode(
+            $request->getContent(),
+            true
+        );
+        $name = $data["name"];
+
+        if (!empty($name)) {
+            $status->setName($name);
+            $statusUsersRepository->save($status, true);
+            return new JsonResponse(
+                [
+                    'message' => "Status is updated",
+                ], Response::HTTP_OK
+            );
+        } else {
+            return new JsonResponse('Name of option is empty with no data',Response::HTTP_BAD_REQUEST);
+        }
+    }
+}
