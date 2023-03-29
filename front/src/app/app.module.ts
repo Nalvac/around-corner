2b5cf9b26864ef7a9b32c5631af54025e62db22d
RobinCoblentz <82441417+RobinCoblentz@users.noreply.github.com>
2023-03-29 11:35:30 +0200

    up

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProfilUserComponent } from './profil-user/profil-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProfilUserComponent
   ],
   imports: [
-    BrowserModule
-
+    BrowserModule,
+    FlexLayoutModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
